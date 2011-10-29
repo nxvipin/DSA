50e32d33042fd3dc0056cd56c5cb575eb2c807bd
Vipin Nair <swvist@gmail.com>
2011-10-30 03:12:44 +0530

    Added rightRotate() method definition

--- libraries/rbtlib.cpp
+++ libraries/rbtlib.cpp
@@ -30,3 +30,24 @@ void rbt::leftRotate(int x){
 	node[x].parent = y;
 }
 
+void rbt::rightRotate(int x){
+	int y;
+	y = node[x].left;
+	node[x].left = node[y].right;
+	if(node[y].right != NIL){
+		node[node[y].right].parent = x;
+	}
+	node[y].parent=node[x].parent;
+	if(node[x].parent == NIL){
+		root = y;
+	}
+	else if(x == node[node[x].parent].right){
+		node[node[x].parent].right = y;
+	}
+	else{
+		node[node[x].parent].left = y;
+	}
+	node[y].right = x;
+	node[x].parent = y;
+}
+
