19b677151e6376827c051207dffd0263c9b3b738
Vipin Nair <swvist@gmail.com>
2011-10-30 02:31:24 +0530

    Added leftRotate() definition

--- libraries/rbtlib.cpp
+++ libraries/rbtlib.cpp
@@ -9,3 +9,24 @@ rbt::~rbt(){
 	delete [] this->node;
 }
 
+void rbt::leftRotate(int x){
+	int y;
+	y = node[x].right;
+	node[x].right = node[y].left;
+	if(node[y].left != NIL){
+		node[node[y].left].parent = x;
+	}
+	node[y].parent=node[x].parent;
+	if(node[x].parent == NIL){
+		root = y;
+	}
+	else if(x == node[node[x].parent].left){
+		node[node[x].parent].left = y;
+	}
+	else{
+		node[node[x].parent].right = y;
+	}
+	node[y].left = x;
+	node[x].parent = y;
+}
+
