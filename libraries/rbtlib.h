17d5611735700e653d7d2f5b1a79fbf78352b5c2
Vipin Nair <swvist@gmail.com>
2011-10-31 04:37:49 +0530

    Changed nodes from array to vector, added createNode()

--- libraries/rbtlib.h
+++ libraries/rbtlib.h
@@ -1,24 +1,24 @@
 /**
 * Red Black Tree
 */
-
+#include <vector>
+using namespace std;
 #define NIL -1
 #define RED 0
 #define BLACK 1
 
 class rbt{
-	private:
-		//Number of keys
-		int n;
 	public:
 		class rbtNode{
 			public:
 				int color, key, left, right, parent;
+				rbtNode(int);
 		};
 		int root;
-		rbtNode *node;
-		rbt(int);
+		vector <rbtNode> node;
+		rbt();
 		~rbt();
+		void createNode(int);
 		void leftRotate(int);
 		void rightRotate(int);
 };
