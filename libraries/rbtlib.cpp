537de2bc81977c9f5d7dc08965d0ebbb98e6bdf2
Vipin Nair <swvist@gmail.com>
2011-11-02 02:56:47 +0530

    Added method definition for insert and insertFix

--- libraries/rbtlib.cpp
+++ libraries/rbtlib.cpp
@@ -3,18 +3,103 @@
 using namespace std;
 rbt::rbt(){
 	root=NIL;
+	createNode(root);
+	node[root].parent=NIL;
+	node[root].left=NIL;
+	node[root].right=NIL;
+	node[root].key=NIL;
 }
 
 rbt::~rbt() {}
 
 rbt::rbtNode::rbtNode(int key){
 	this->key = key;
-	this->color = RED;
 }
 
 int rbt::createNode(int key){
 	node.push_back(key);
 	return node.size()-1;
+}
+
+void rbt::insert(int key){
+	int x,y,z;
+	z=createNode(key);
+	y = NIL;
+	x = root;
+	while(x != NIL){
+		y=x;
+		if(node[z].key < node[x].key){
+			x=node[x].left;
+		}
+		else{
+			x=node[x].right;
+		}
+	}
+	node[z].parent=y;
+	if(y == NIL){
+		root = z;
+	}
+	else if(node[z].key < node[y].key){
+		node[y].left = z;
+	}
+	else{
+		node[y].right = z;
+	}
+	node[z].left = NIL;
+	node[z].right = NIL;
+	node[z].color = RED;
+	insertFix(z);
+}
+
+void rbt::insertFix(int z){
+	int y;
+	char tmp;
+	while(node[node[z].parent].color == RED && node[z].parent != NIL ){
+		if(node[z].parent == node[node[node[z].parent].parent].left){
+			y = node[node[node[z].parent].parent].right;
+			if(node[y].color == RED){
+				node[node[z].parent].color = BLACK;
+				node[y].color = BLACK;
+				node[node[node[z].parent].parent].color = RED;
+				z = node[node[z].parent].parent;
+			}
+			else if(z = node[node[z].parent].right){
+				z = node[z].parent;
+				leftRotate(z);
+			}
+			if(node[z].parent != NIL){
+				node[node[z].parent].color = BLACK;
+			}
+			if(node[node[z].parent].parent != NIL){
+				node[node[node[z].parent].parent].color = RED;
+				rightRotate(node[node[z].parent].parent);
+			}
+			node[root].color=BLACK;
+		}
+		else{
+			y = node[node[node[z].parent].parent].left;
+			if(node[y].color == RED){
+				node[node[z].parent].color = BLACK;
+				node[y].color = BLACK;
+				if(node[node[z].parent].parent != NIL){
+				node[node[node[z].parent].parent].color = RED;
+				}
+				z = node[node[z].parent].parent;
+			}
+			else if(z = node[node[z].parent].left){
+				z = node[z].parent;
+				rightRotate(z);
+			}
+			if(node[z].parent != NIL){
+				node[node[z].parent].color = BLACK;
+			}
+			if(node[node[z].parent].parent != NIL){
+				node[node[node[z].parent].parent].color = RED;
+				leftRotate(node[node[z].parent].parent);
+			}
+		}
+	}
+	node[root].color = BLACK;
 }
 
 void rbt::leftRotate(int x){
